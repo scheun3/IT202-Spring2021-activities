33d55a176223893bdf7117c613cd84e1d9f4351c
Stan Cheung <scheun3@uic.edu>
2021-03-01 01:52:10 +0000

    Finished Homework 7

---
+++ Homework 7/canvas.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var canvas = document.getElementById("html-canvas");
+  var circles = [];
+  var radius = 50;
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  var context = canvas.getContext("2d");
+}, false);
